96c1997c3d655038894da3cee20c1126d2707421
adityacaturputra <adityacaturputra25@gmail.com>
2025-12-27 15:21:09 +0700

    remove service worker and fix the search templates data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -118,23 +118,6 @@ export default function RootLayout({
             }),
           }}
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              if ('serviceWorker' in navigator) {
-                window.addEventListener('load', function() {
-                  navigator.serviceWorker.register('/sw.js')
-                    .then(function(registration) {
-                      console.log('SW registered: ', registration);
-                    })
-                    .catch(function(registrationError) {
-                      console.log('SW registration failed: ', registrationError);
-                    });
-                });
-              }
-            `,
-          }}
-        />
       </head>
       <body className='antialiased'>
         <ThemeProvider>
