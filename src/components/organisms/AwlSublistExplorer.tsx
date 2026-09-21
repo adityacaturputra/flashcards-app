7e7619f420eef1c2cf8c221151523f771c1f8299
adityacaturputra <adityacaturputra25@gmail.com>
2026-09-21 22:56:52 +0700

    fix(awl): render all 10 sublist filter options in AwlSublistExplorer

--- src/components/organisms/AwlSublistExplorer.tsx
+++ src/components/organisms/AwlSublistExplorer.tsx
@@ -132,7 +132,7 @@ export const AwlSublistExplorer: React.FC<AwlSublistExplorerProps> = ({
               Semua Sublist
             </button>
 
-            {[1, 2, 3, 7].map((num) => {
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((num) => {
               const isSelected = selectedSublist === num;
               return (
                 <button
